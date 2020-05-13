6a13801ee32d9a20eeba81f4770e4a126f025fad
Dustin Ancalade <umbi24@gmail.com>
2020-05-12 22:36:58 -0700

    boilerplate for server react and webpack setup complete

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const path = require('path');
 
+const app = express();
 
+const port = process.env.PORT || 4400;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendStatus(200).end();
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on ${port} `);
+})
