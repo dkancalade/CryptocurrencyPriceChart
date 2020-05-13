d878741d985edfe7af9e61c3462109b08955fbdb
Dustin Ancalade <umbi24@gmail.com>
2020-05-12 22:23:06 -0700

    boilerplate for webpack and front end complete but untested

---
+++ challenge_2/client/components/app.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={};
+  }
+
+  render() {
+    return (
+      <div>React is working now, yay!</div>
+    );
+  }
+}
+
+export default App;
